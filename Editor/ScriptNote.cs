50807a592220f4cdf41e093c409d0a11ce2581fd
AnEmortalKid <janmonterrubio@gmail.com>
2025-01-19 23:30:22 -0600

    add initial impl

---
+++ Editor/ScriptNote.cs
@@ -0,0 +1,11 @@
+﻿namespace Snoutical.ScriptNotes
+{
+    /// <summary>
+    /// Combines the note and an identifier for the script for caching purposes on the same object
+    /// </summary>
+    public class ScriptNote
+    {
+        public string ScriptName;
+        public string Note;
+    }
+}
